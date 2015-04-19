01646a3a3c99f8433e37dcd5638a623d174d536f
AragaoAnderson <andersonaragao@me.com>
2015-04-19 01:29:59 -0300

    Victara: Update Attentive display
    Change-Id: I94f4162c4ed07a42a9c69eb17a9993ffda42d7b7

--- nxactions/src/com/cyanogenmod/settings/device/AttentiveDisplay.java
+++ nxactions/src/com/cyanogenmod/settings/device/AttentiveDisplay.java
@@ -31,10 +31,12 @@ import static com.cyanogenmod.settings.device.IrGestureManager.*;
 public class AttentiveDisplay implements ActionableSensor {
     private static final String TAG = "CMActions-AD";
 
-    private static final int IR_GESTURES_FOR_SCREEN_ON = (1 << IR_GESTURE_OBJECT_DETECTED) | (1 << IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED);
+    private static final int IR_GESTURES_FOR_SCREEN_ON = (1 << IR_GESTURE_OBJECT_DETECTED) |
+            (1 << IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED);
     private static final int IR_GESTURES_FOR_SCREEN_OFF = 0;
     private static final String GESTURE_IR_ATTENTIVE_KEY = "gesture_attentive_display";
-    private static final int DELAY_MS = 5000;
+
+    private static final int DELAYED_OFF_MS = 3000;
 
     private final SensorHelper mSensorHelper;
     private final IrGestureVote mIrGestureVote;
@@ -44,8 +46,10 @@ public class AttentiveDisplay implements ActionableSensor {
 
     private boolean mEnabled;
     private boolean mScreenIsLocked;
+    private boolean mObjectIsDetected;
+    private boolean mIsStowed;
     private WakeLock mWakeLock;
-    private long mSensorStartedMs;
+    private WakeLock mDelayedOffWakeLock;
 
     private Context mContext;
 
@@ -59,6 +63,7 @@ public class AttentiveDisplay implements ActionableSensor {
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
         mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, TAG);
+        mDelayedOffWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, TAG);
 
         mIrGestureSensor = sensorHelper.getIrGestureSensor();
         mStowSensor = sensorHelper.getStowSensor();
@@ -90,27 +95,53 @@ public class AttentiveDisplay implements ActionableSensor {
     private void enableSensor() {
         if (!mEnabled) {
             Log.d(TAG, "Enabling");
-            mSensorHelper.unregisterListener(mIrGestureListener);
-            mSensorHelper.registerListener(mIrGestureSensor, mIrGestureListener);
-            mIrGestureVote.voteForState(true, IR_GESTURES_FOR_SCREEN_OFF);
+
             mEnabled = true;
-            mSensorStartedMs = System.currentTimeMillis();
+            mObjectIsDetected = false;
+            mIsStowed = false;
+
+            mSensorHelper.registerListener(mIrGestureSensor, mIrGestureListener);
+            mSensorHelper.registerListener(mStowSensor, mStowListener);
+            mIrGestureVote.voteForState(true, IR_GESTURES_FOR_SCREEN_ON);
+
         }
     }
 
     private void disableSensor() {
         if (mEnabled) {
             Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(mStowListener);
             mSensorHelper.unregisterListener(mIrGestureListener);
-            mIrGestureVote.voteForState(false, IR_GESTURES_FOR_SCREEN_ON);
+            mIrGestureVote.voteForState(false, IR_GESTURES_FOR_SCREEN_OFF);
             mEnabled = false;
+        }
+    }
+
+    private synchronized void setIsStowed(boolean isStowed) {
+        Log.d(TAG, "Stowed: " + isStowed);
+        mIsStowed = isStowed;
+        updateScreenLock();
+    }
+
+    private synchronized void setObjectIsDetected(boolean objectIsDetected) {
+        Log.d(TAG, "IR object is detected: " + objectIsDetected);
+        mObjectIsDetected = objectIsDetected;
+        updateScreenLock();
+    }
+
+    private synchronized void updateScreenLock() {
+        boolean isLocked = mObjectIsDetected && !mIsStowed;
+
+        if (isLocked) {
+            enableScreenLock();
+        } else {
+            disableScreenLock();
         }
     }
 
     private synchronized void enableScreenLock() {
         if (!mScreenIsLocked) {
             Log.d(TAG, "Acquiring screen wakelock");
-            mSensorHelper.registerListener(mStowSensor, mStowListener);
             mScreenIsLocked = true;
             mWakeLock.acquire();
         }
@@ -118,8 +149,8 @@ public class AttentiveDisplay implements ActionableSensor {
 
     private synchronized void disableScreenLock() {
         if (mScreenIsLocked) {
-            mSensorHelper.unregisterListener(mStowListener);
             mScreenIsLocked = false;
+            mDelayedOffWakeLock.acquire(DELAYED_OFF_MS);
             mWakeLock.release();
             Log.d(TAG, "Released screen wakelock");
         }
@@ -131,10 +162,10 @@ public class AttentiveDisplay implements ActionableSensor {
             int gesture = (int) event.values[1];
 
             if (gesture == IR_GESTURE_OBJECT_DETECTED && mGestureIrAttentiveEnabled) {
-                enableScreenLock();
+                setObjectIsDetected(true);
             } else if (gesture == IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED) {
                 Log.d(TAG, "object is gone");
-                disableScreenLock();
+                setObjectIsDetected(false);
             }
         }
 
@@ -146,18 +177,8 @@ public class AttentiveDisplay implements ActionableSensor {
     private SensorEventListener mStowListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            boolean isStowed = (event.values[0] != 0);
+            setIsStowed(event.values[0] != 0);
 
-            long now = System.currentTimeMillis();
-            if (now - mSensorStartedMs <= DELAY_MS) {
-                if (isStowed) {
-                    Log.d(TAG, "stowed");
-                    disableScreenLock();
-                }
-            } else {
-                Log.d(TAG, "ignoring stow sensor");
-                mSensorHelper.unregisterListener(mStowListener);
-            }
         }
 
         @Override
