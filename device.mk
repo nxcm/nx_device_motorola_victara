2291fad2c17449a1c43f626e6542cac9575ef100
AragaoAnderson <andersonaragao@me.com>
2015-05-04 21:27:53 -0300

    Victara: More changes on NX
    Changes for Doze and Screen density
    
    Change-Id: Id92c0dba167c671976b8451fa118924fc4335900

--- device.mk
+++ device.mk
@@ -54,9 +54,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
