9e29352e2983288197f69fd9dcca932deb8ecaa6
AragaoAnderson <andersonaragao@me.com>
2015-05-17 01:56:05 -0300

    Victara: Attempt to fix not compatible apps
    Change-Id: I9b6c00f92f99e539c81cda92984654b4b2ce363a

--- device.mk
+++ device.mk
@@ -52,8 +52,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-PRODUCT_CHARACTERISTICS := nosdcard
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
