a09860de22e8326fdb02d73162be377e7a2c5cad
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-19 19:59:34 -0500

    overlay: select ipv6 apn by default on t-mobile
    * also ensure overlay overrides anything in vendor/cm
    
    Change-Id: If63072a68fd538a16674329a90d92ebb4e964c9a

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := nx_victara
 NX_MODEL_NAME := Moto X (2014)
