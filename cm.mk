f746b6b3c9fa38e130aa1563243da000339d5031
AragaoAnderson <andersonaragao@me.com>
2015-08-21 11:06:44 -0300

    Victara: Our build is nx_
    Change-Id: Ic311ce8960744fb9fd25b7c1458e95f5c34878e7

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := cm_victara
+PRODUCT_NAME := nx_victara
 NX_MODEL_NAME := Moto X (2014)
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
